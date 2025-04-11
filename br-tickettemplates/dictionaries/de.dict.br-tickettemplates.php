eb1a66ff4082e322e746ebdec7806809409bd952
Bjoern Rudner <rudnerbjoern@hotmail.com>
2025-04-11 10:24:35 +0200

    first test

---
+++ br-tickettemplates/dictionaries/de.dict.br-tickettemplates.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2025 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2025-04-11
+ *
+ * Localized data
+ */
+
+//
+// Ticket Templates
+//
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'Menu:RoutineChangeTemplates' => 'Routine Change Vorlagen',
+    'Menu:RoutineChangeTemplates+' => '',
+));
