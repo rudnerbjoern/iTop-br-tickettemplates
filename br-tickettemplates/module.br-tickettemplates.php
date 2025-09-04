218f77b5385d89734865d1944b2179073923e316
Björn Rudner <rudnerbjoern@hotmail.com>
2025-09-04 12:40:19 +0200

    set version 0.0.3

--- br-tickettemplates/module.br-tickettemplates.php
+++ br-tickettemplates/module.br-tickettemplates.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2025-06-12
+ * @version     2025-09-04
  *
  * iTop module definition file
  */
@@ -11,7 +11,7 @@
 /** @disregard P1009 Undefined type SetupWebPage */
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-tickettemplates/0.0.2',
+    'br-tickettemplates/0.0.3',
     array(
         // Identification
         //
